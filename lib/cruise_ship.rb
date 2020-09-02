80221c912533741e844e74cbbf0a1e847f6a425b
Johnson Liu <jyliu17@gmail.com>
2020-09-02 00:09:24 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,10 @@
 def select_winner(passengers)
   # add the code snippet here!
 end 
- winner=""
+ winner =""
  passengers.each do |room, name|
-  if room==:suite_a && name.start_with?("A")
+  if room == :suite_a && name.start_with?("A")
     winner=name
   end
 end
 winner
-end
