2d68250f2543b5141127495f0c3b6f11da4d3d52
Johnson Liu <jyliu17@gmail.com>
2020-09-02 00:14:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,6 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   
   birthday_kids.each do |name, age|
-    puts "Happy birthday #{name}! Your are #{age} years old!"
+    puts "Happy birthday #{name}! You are now #{age} years old!"
   end
 end
